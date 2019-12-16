45d7272369bd4fb59ae3cb6582d56ba9ea58bf03
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2019-12-15 22:06:36 -0600

    Updates

---
+++ precache-manifest.408d572739f06535592219e330a0a6c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1980deb44b62adf748f2ae3f283f3108",
+    "url": "/WEB_API_REACT/index.html"
+  },
+  {
+    "revision": "bca696f9f73855bddfd5",
+    "url": "/WEB_API_REACT/static/css/main.1bc07143.chunk.css"
+  },
+  {
+    "revision": "9a61b4cef45e560af816",
+    "url": "/WEB_API_REACT/static/js/2.08815c24.chunk.js"
+  },
+  {
+    "revision": "bca696f9f73855bddfd5",
+    "url": "/WEB_API_REACT/static/js/main.c8e5ec89.chunk.js"
+  },
+  {
+    "revision": "85a2862332abee502aad",
+    "url": "/WEB_API_REACT/static/js/runtime-main.e50d1c79.js"
+  },
+  {
+    "revision": "e86d97c08805ce75a6e83bbd1c272455",
+    "url": "/WEB_API_REACT/static/media/logo.e86d97c0.png"
+  }
+]);
